f4fb1e626056f7be36228be03b3992a67b731924
audun-web <audun@midtmeck.com>
2025-10-17 13:40:50 +0200

    integrerte jinja variabler og en for løkke i /meny siden

--- app.py
+++ app.py
@@ -8,7 +8,44 @@ def home():
 
 @app.route('/meny')
 def meny():
-    return render_template('meny.html')
+    menu_data = [
+        {
+            "day": "Mandag",
+            "image": "img/kremet-pasta.webp",
+            "alt_text": "Kremet pasta med kylling",
+            "description": "Kremet pasta med kylling og soltørkede tomater.",
+            "price": "45,-"
+        },
+        {
+            "day": "Tirsdag",
+            "image": "img/pannestekt-laks.webp",
+            "alt_text": "Pannestekt laks",
+            "description": "Pannestekt laks servert med ovnsbakte rotgrønnsaker.",
+            "price": "50,-"
+        },
+        {
+            "day": "Onsdag",
+            "image": "img/lasagne.jpg",
+            "alt_text": "Hjemmelaget lasagne",
+            "description": "Hjemmelaget lasagne med frisk salat og hvitløksbrød.",
+            "price": "45,-"
+        },
+        {
+            "day": "Torsdag",
+            "image": "img/burger.jpeg",
+            "alt_text": "Saftig burger",
+            "description": "Burger-torsdag! Saftig burger med cheddar og bacon.",
+            "price": "48,-"
+        },
+        {
+            "day": "Fredag",
+            "image": "img/taco.webp",
+            "alt_text": "Taco-buffet",
+            "description": "Taco-fredag! Buffet med alt du kan ønske deg av tilbehør.",
+            "price": "49,-"
+        }
+    ]
+    return render_template('meny.html', menu_items=menu_data)
 
 @app.route('/varer')
 def varer():
